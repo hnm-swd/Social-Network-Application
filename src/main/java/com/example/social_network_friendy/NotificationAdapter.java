1188d55b5d9cd5739b5a03d71f07cec53e1ffda0
Cẩm Trâm <124569404+hnm-swd@users.noreply.github.com>
2024-10-16 20:36:52 +0700

    Update NotificationAdapter.java

--- src/main/java/com/example/social_network_friendy/NotificationAdapter.java
+++ src/main/java/com/example/social_network_friendy/NotificationAdapter.java
@@ -1,6 +1,9 @@
 package com.example.social_network_friendy;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,29 +13,23 @@ import java.util.List;
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
     private List<NotificationItem> notificationList;
 
+    // Constructor
     public NotificationAdapter(List<NotificationItem> notificationList) {
         this.notificationList = notificationList;
-    }
-
-    public NotificationAdapter(List<NotificationItem> notificationList) {
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the layout for each notification item
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_notification, parent, false);
         return new ViewHolder(view);
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Bind the data to the views
         NotificationItem notification = notificationList.get(position);
         holder.titleTextView.setText(notification.getTitle());
         holder.timeTextView.setText(notification.getTime());
@@ -43,9 +40,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         return notificationList.size();
     }
 
-    public void notifyDataSetChanged() {
-    }
-
+    // ViewHolder class to hold references to each item's views
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView titleTextView;
         public TextView timeTextView;
@@ -57,4 +52,3 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         }
     }
 }
-
